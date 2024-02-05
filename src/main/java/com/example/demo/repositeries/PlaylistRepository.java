dffe784a3404c259ddad7eae84638a71c1e75537
Pavan Kumar Vollu <Pavan Kumar Vollu@LAPTOP-8JCFN69B>
2024-02-04 19:31:17 -0800

    1st version of tune hub

---
+++ src/main/java/com/example/demo/repositeries/PlaylistRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repositeries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.Playlist;
+
+public interface PlaylistRepository 
+           extends JpaRepository<Playlist, Integer>{
+
+}
